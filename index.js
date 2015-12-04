ea4a0d7e81ea5ce4f640824f15612c8eb0cd26b3
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2015-12-04 09:21:32 -0600

    capslock constant

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var request = require ('request');
 
-var apiURL = 'https://api.pinboard.in/v1/';
+var API_URL = 'https://api.pinboard.in/v1/';
 
 function pinboardMethod(endpoint, singleOption) {
   return function(opts, cb) {
-    var url = apiURL + endpoint;
+    var url = API_URL + endpoint;
 
     if (singleOption) {
       var optsObj = {};
